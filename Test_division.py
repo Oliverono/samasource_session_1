8dcc4d1b7d7cd4e2f49c89ffdd4349504bf30532
oliverono <olivkemb@gmail.com>
2017-10-14 11:43:58 +0300

    refactor tests

--- Test_division.py
+++ Test_division.py
@@ -1,29 +1,30 @@
 import unittest
+from devide_numbers import divide_numbers
 #Test Cases
 
-# Divide two numbers
-def test_divide_two_numbers(self):
-    result = divide_numbers(4,2)
-    self.assertEqual(result,2)
+class DivideTestCases(unittest.TestCase):
+    # Divide two numbers
+    def test_divide_two_numbers(self):
+        result = divide_numbers(4,2)
+        self.assertEqual(result,2)
 
-#Divide by Zero
-def test_divide_by_zero(self):
-  
-    with self.assertRaises(ZeroDivisionError)
-      divide_numbers(4,0)
+    #Divide by Zero
+    def test_divide_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            divide_numbers(4,0)
 
-# Divide two strings
-def test_divide_two_strings(self):
-   
-    with self.assertRaises(TypeError)
-      divide_numbers(4,0)
-
-#Divide two arrays
-def test_divide_two_arrays(self):
+    # Divide two strings
+    def test_divide_two_strings(self):
     
-    with self.assertRaises(TypeError, result)
-      divide_numbers([1],[2])
+        with self.assertRaises(TypeError):
+            divide_numbers('4','0')
 
-if __name__ = "__main__":
+    #Divide two arrays
+    def test_divide_two_arrays(self):
+        
+        with self.assertRaises(TypeError):
+            divide_numbers([1],[2])
+
+if __name__ == "__main__":
      unittest.main()   
 #  
